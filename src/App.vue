0ff35e3c2c3b5d9d3d8300d8a0442815bc0c9035
João Vitor Andrade Santos <asauskyboladaum@gmail.com>
2025-03-21 11:12:50 -0300

    Desafio finalizado

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <router-view>
+
+    </router-view>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+};
 </script>
 
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
